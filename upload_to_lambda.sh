52d137ff9797fb7ba24f0f77a2416c39214bffd2
Vikas Saini <vikassaini480@gmail.com>
2020-07-14 12:38:34 +0530

    upload_to_lambda.sh updated

--- upload_to_lambda.sh
+++ upload_to_lambda.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-#pipenv install
-#venv_path=`pipenv --venv`
-#cp -r $venv_path/lib/python3.6/site-packages ./
-
-zip -r function.zip ./*
-scp -i ~/.ssh/pe-jenkins.pem function.zip ubuntu@52.16.177.114:
-#aws lambda update-function-code --function-name business_reporting --zip-file fileb://function.zip
+pipenv install
+venv_path=`pipenv --venv`
+mkdir deploy_dir
+cp -r ./* deploy_dir/
+rm -r deploy_dir/deploy_dir
+cp -r $venv_path/lib/python3.6/site-packages/* deploy_dir/
+cd deploy_dir/
+zip -r ../function.zip ./*
+cd ..
+aws s3 cp function.zip s3://pure-artifactory/business_reporting/
+aws lambda update-function-code \
+        --function-name  business_reporting \
+        --s3-bucket pure-artifactory \
+        --s3-key business_reporting/function.zip
+rm -r deploy_dir
